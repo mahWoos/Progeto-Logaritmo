ec1308582292e12b9ad6ce52951e666c7e45ed79
mahWoos <110110448+mahWoos@users.noreply.github.com>
2024-07-24 11:53:00 -0300

    Add files via upload

--- Progeto-Logaritmo-main/Progeto-Logaritmo-main/script.js
+++ Progeto-Logaritmo-main/Progeto-Logaritmo-main/script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Como os logaritmos são usados na análise de crescimento populacional?",
+        enunciado: "Como os logaritmos são usados na ánalise de crescimento populacional?",
         alternativas: [
             {
                 texto: "Os logaritmos são usados para calcular a taxa de natalidade direta.",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: " "
             },
             {
                 texto: "Os logaritmos ajudam a modelar o crescimento exponencial da população.",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: ""
             }
         ]
     },
@@ -22,25 +22,51 @@ const perguntas = [
         enunciado: "Como os logaritmos são utilizados para medir a intensidade de luz estelar em astronomia?",
         alternativas: [
             {
-                texto: "Os logaritmos são usados para calcular a magnitude aparente das estrelas, comparando seu brilho com uma estrela de referência.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Os logaritmos são usados para calcular a magnitude aparente das estrelas,comparando seu brilho com uma estrela de referência.",
+                afirmacao: ""
             },
             {
-                texto: "Os logaritmos são usados para medir a distância entre estrelas.",
+                texto: "Os logaritmos",
                 afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Qual é a relação entre logaritmos e juros compostos na economia?",
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "Os logaritmos não tem aplicação em cálculos financeiros.",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-                texto: "Os logaritmos são usados para calcular o número de períodos necessários para um investimento crescer a um valor específico.",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
     },
@@ -79,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Conclusão";
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
